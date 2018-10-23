5846f8dc93932e103a965ce3b36d5f63cd4c9328
Alex Baranov <a.b320012@gmail.com>
2018-10-23 16:55:52 -0400

    changes
    Former-commit-id: 6d8187ecb9e2787e36f6d2cc25f9fe951ac51076
    Former-commit-id: 876c9050031d93c549334aaf42150d7467879fd6

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,12 +63,12 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'http://virtual-ambler.herokuapp.com',
+    :domain => 'virtual-ambler.herokuapp.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
