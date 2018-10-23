0139c4a793635392934d73406bc3f183336cbd79
Alex Baranov <a.b320012@gmail.com>
2018-10-23 15:01:58 -0400

    mailing method
    Former-commit-id: 10e3193201c556d0df576733936455a6729794ca
    Former-commit-id: 0e9e484b1f298df3d2263466516477ffb06221bc

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,20 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "vambler_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'virtualsilcton.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+  ActionMailer::Base.delivery_method = :smtp
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
