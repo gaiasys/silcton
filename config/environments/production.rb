01a6c8bbdb8abcd535456b0b98d03f9464adbc56
Alex Baranov <a.b320012@gmail.com>
2018-10-23 16:44:05 -0400

    fixing email
    Former-commit-id: 0fc2fcbce28953b276bc5c0ec7434175835574e3
    Former-commit-id: 26993fd7ee3414f7eba2ab056eff1b6a0b597485

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'virtualsilcton.com',
+    :domain => 'http://virtual-ambler.herokuapp.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
