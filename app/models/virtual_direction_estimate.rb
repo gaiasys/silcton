aaef21c3fbd880842326267882fe6310f2b2a2ff
Steve <stevenweisberg@ufl.edu>
2020-02-27 10:18:01 -0500

    bug squashing

--- app/models/virtual_direction_estimate.rb
+++ app/models/virtual_direction_estimate.rb
@@ -5,38 +5,45 @@ class VirtualDirectionEstimate < ApplicationRecord
   belongs_to :target_landmark, :class_name => "VirtualLandmark"
 
   def true_direction
-    # from pointing location to front door
-    # http://en.wikipedia.org/wiki/Law_of_cosines#Applications
-    # C = start landmark pointing location, B = target landmark front door, A = facing_landmark pointing location
-    #a = start_landmark.distance_to(facing_landmark, "pointing_location", "pointing_location") #CA
-    #b = start_landmark.distance_to(target_landmark, "pointing_location", "front_door") #CB
-    #c = target_landmark.distance_to(facing_landmark, "front_door", "pointing_location") #BA
-    #angle = Math.acos((a**2 + b**2 - c**2) / (2 * a * b))
-    #
+      # from pointing location to front door
+      # http://en.wikipedia.org/wiki/Law_of_cosines#Applications
+      # C = start landmark pointing location, B = target landmark front door, A = facing_landmark pointing location
+      #a = start_landmark.distance_to(facing_landmark, "pointing_location", "pointing_location") #CA
+      #b = start_landmark.distance_to(target_landmark, "pointing_location", "front_door") #CB
+      #c = target_landmark.distance_to(facing_landmark, "front_door", "pointing_location") #BA
+      #angle = Math.acos((a**2 + b**2 - c**2) / (2 * a * b))
 
-    facing = Matrix[[facing_landmark.pointing_location_pixel_x,facing_landmark.pointing_location_pixel_y]]
-    point_from = Matrix[[start_landmark.pointing_location_pixel_x,start_landmark.pointing_location_pixel_y]]
-    target = Matrix[[start_landmark.front_door_pixel_x,start_landmark.front_door_pixel_y]]
-    #vector subtraction logic from here:
-    #https://stackoverflow.com/questions/21483999/using-atan2-to-find-angle-between-two-vectors
-    vector1 = facing-point_from # center the facing and target points
-    vector2 = target-point_from
-    # Now calculate the signed angle between them as the difference between arctans (y,x)
-    angle = Math.arctan2(vector1[0,1],vector1[0,0]) - np.arctan2(vector2[0,1],vector2[0,0])
-    return rad2deg(angle)
 
-  end
+      facing = [facing_landmark.pointing_location_pixel_x,facing_landmark.pointing_location_pixel_y]
+      point_from = [start_landmark.pointing_location_pixel_x,start_landmark.pointing_location_pixel_y]
+      target = [start_landmark.front_door_pixel_x,start_landmark.front_door_pixel_y]
+      #vector subtraction logic from here:
+      #https://stackoverflow.com/questions/21483999/using-atan2-to-find-angle-between-two-vectors
+      vector1 = point_from.zip(facing).map { |x, y| y - x } # center the facing and target points
+      vector2 = point_from.zip(target).map { |x, y| y - x }
+      # Now calculate the signed angle between them as the difference between arctans (y,x)
+      angle = Math.atan2(vector1[1],vector1[0])-Math.atan2(vector2[1],vector2[0])
+      angle = angle * 180 / Math::PI
+      if angle > 180
+            correctedAngle = angle - 360
+        elsif angle < -180
+            correctedAngle = 360 + angle
+        else
+            correctedAngle = angle
 
-  def abs_error
-    abs_error = (bearing - true_direction).modulo(360)
-    if abs_error > 180
-      abs_error = 360 - abs_error
-    else
-      abs_error
+      return angle
+
+    end
+
+    def abs_error
+      abs_error = (bearing - true_direction).modulo(360)
+      if abs_error > 180
+        abs_error = 360 - abs_error
+      else
+        abs_error
+      end
     end
   end
 
-  def rad2deg(r)
-    r * 180 / Math::PI
-  end
+
 end
