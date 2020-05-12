6444be9aac16d874fdcfb19e3da8a4adb074ab56
kapilchoudhary <kapil92kumarpatel@gmail.com>
2020-05-12 14:18:23 +0530

    questions form the UI removed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200427123809) do
+ActiveRecord::Schema.define(version: 20200512075826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
