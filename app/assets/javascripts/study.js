3dd74b1d14212c0e193291b6b0f7fa38251a396c
Subrahmanyam Bhamidipati <bhsubbu.1995@gmail.com>
2021-10-25 04:02:43 -0700

    fix: build and minor errors

--- app/assets/javascripts/study.js
+++ app/assets/javascripts/study.js
@@ -1,21 +1,23 @@
 $(document).ready(function () {
-  $("#study-participants-table input[id='pilot-subject']").change(async function () {
+  $("#study-participants-table input[id='pilot-subject']").change(function () {
     var participant_id = $(this).closest("tr").attr("data-id");
     $.ajax({
       type: "PUT",
       url: "experimenters/participants/" + participant_id,
       data: { participant: { pilot_subject: this.checked } },
+    }).then(function() {
+      alert('Particpant updated successfully')
     });
-    alert('Particpant updated successfully')
   });
 
-  $("#study-participants-table input[id='participant-id']").blur(async function () {
+  $("#study-participants-table input[id='participant-id']").blur(function () {
     var participant_id = $(this).closest("tr").attr("data-id");
-    await $.ajax({
+    $.ajax({
       type: "PUT",
       url: "experimenters/participants/" + participant_id,
       data: { participant: { identification: this.value } },
+    }).then(function() {
+      alert('Particpant updated successfully')
     });
-    alert('Particpant updated successfully')
   });
 });
