f9dce6525bf5d7e36e7fdeba4b8f19dcf1b33355
Manisha <manisha@21twelveinteractive.com>
2021-11-17 17:53:40 +0530

    Add modify button on view participant

--- app/assets/javascripts/study.js
+++ app/assets/javascripts/study.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).on('turbolinks:load', function() {
   $("#study-participants-table input[id='pilot-subject']").change(function () {
     var participant_id = $(this).closest("tr").attr("data-id");
     $.ajax({
@@ -10,14 +10,16 @@ $(document).ready(function () {
     });
   });
 
-  $("#study-participants-table input[id='participant-id']").blur(function () {
+  $("#study-participants-table .modify-participant-id").on('click',function () {
     var participant_id = $(this).closest("tr").attr("data-id");
+    var identification = $(this).closest("tr").find(".participant-id").val();
     $.ajax({
       type: "PUT",
       url: "experimenters/participants/" + participant_id,
-      data: { participant: { identification: this.value } },
+      data: { participant: { identification: identification} },
     }).then(function() {
       alert('Participant updated successfully')
     });
   });
+
 });
